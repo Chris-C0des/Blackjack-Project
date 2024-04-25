5b3dae63a6a6553fca151d25bbce9df21286afaf
Chris-C0des <152207802+Chris-C0des@users.noreply.github.com>
2024-04-25 12:04:40 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.GreenfootImage;
 
 /**
  * Write a description of class MyWorld here.
@@ -8,19 +9,43 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
-    /**
+    public StartButton start = new StartButton();
+    public StartScreen load = new StartScreen();
+    public Leprechuan lucky = new Leprechuan();
+    GifImage yay = new GifImage("StartScreen.gif");
+    public Title Clove = new Title();
+    public Rainbow rain = new Rainbow();
+    public BlackjackGame game = new BlackjackGame(3,2);
+    
+    
+    
+ /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1100, 800, 1);
-        setBackground("./images/blackjackTable.png");
-        deal ib = new deal();
-        //GreenfootImage image = new GreenfootImage("Lep.png");
-        String sound = "./sounds/BackgroundMusic.mp3";
-        Greenfoot.playSound(sound);
-    }
-}
+    {   
+        super(1100,600,1,false);
+        addObject(load,630,300);
+        addObject(rain,550,430);
+        addObject(start,550,500);
+        addObject(lucky,960,560);
+        addObject(Clove,560,190);
+        addObject(new HitButton(), getWidth() - 800, getHeight() - 50);
+        
+     }
+    
+     public void act(){
+         if(Greenfoot.mousePressed(start)){
+            removeObject(load);
+            removeObject(rain);
+            removeObject(lucky);
+            removeObject(start);
+            removeObject(Clove);
+            addObject(game,500,300);
+            addObject(game,500,300);
+         }
+        }
+     }
+
+
